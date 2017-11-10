186ff6b7bf699cc6f18d6cc5a0360b8586774ee7
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2017-11-09 23:55:08 -0500

    added new mix project to create twitter like enginer

---
+++ project4part1/lib/project4part1.ex
@@ -0,0 +1,18 @@
+defmodule Project4part1 do
+  @moduledoc """
+  Documentation for Project4part1.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Project4part1.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
