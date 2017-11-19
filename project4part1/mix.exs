1f7082bbe6503e986b6b1c03835a5f2ac152bf5b
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2017-11-19 14:35:48 -0500

    added escript config for project

--- project4part1/mix.exs
+++ project4part1/mix.exs
@@ -7,6 +7,7 @@ defmodule Project4part1.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
+      escript: escript_config(),
       deps: deps()
     ]
   end
@@ -24,5 +25,8 @@ defmodule Project4part1.Mixfile do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
+  end
+  defp escript_config do
+    [main_module: Project4part1]
   end
 end
