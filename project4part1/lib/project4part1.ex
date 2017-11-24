cc2cf9d66afbbd137eed1da9e03650cbb991fa62
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2017-11-24 02:17:27 -0500

    accepting command line arguments to run the code as server or client

--- project4part1/lib/project4part1.ex
+++ project4part1/lib/project4part1.ex
@@ -12,7 +12,22 @@ defmodule Project4part1 do
       :world
 
   """
-  def hello do
-    :world
+  require Logger
+  def main(args) do
+    {_, [str], _} = OptionParser.parse(args)
+    server_ip = parse_ip("127.0.0.1")
+    port = 6666
+    if str == "server" do
+      Logger.debug "Starting as Server"
+      Server.start_link(port)
+    else
+      Logger.debug "Starting as Client"
+      Client.start_link(server_ip, port)
+    end
+  end
+  defp parse_ip(str) do
+    # convert input string 127.0.0.1 to tuple of integers like {127, 0, 0, 1}
+    [a, b, c, d] = String.split(str, ".")
+    {String.to_integer(a), String.to_integer(b), String.to_integer(c), String.to_integer(d)}
   end
 end
