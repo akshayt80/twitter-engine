d865725bcf2ec3afd77e1ba11e6a514d0cac4a43
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2017-11-26 03:50:18 -0500

    added some utility funnctions

--- project4part1/lib/client.ex
+++ project4part1/lib/client.ex
@@ -33,7 +33,7 @@ defmodule Client do
     end
 
     defp receive_message(receiver) do
-        {:ok, data} = :gen_tcp.recv(socket, 0)
+        {:ok, data} = :gen_tcp.recv(receiver, 0)
         Poison.decode!(data)
     end
 
@@ -68,11 +68,32 @@ defmodule Client do
         {:noreply, map}
     end
 
-    def handle_cast({:tweet}, map) do
+    def handle_cast({:tweet, username, sender, tweet, socket}, map) do
+        Logger.debug "user: #{username} sender: #{sender} tweet: #{tweet}"
+        received_tweet(socket, username, tweet)
         {:noreply, map}
     end
 
     ####################
+
+    defp listen(socket, username) do
+        {:ok, data} = :gen_tcp.recv(socket, 0)
+        data = Poison.decode!(data)
+        Logger.debug "received data from worker #{inspect(worker)} data: #{inspect(data)}"
+
+        # Send value of k as String
+        #Logger.debug "sending initial message"
+        #:gen_tcp.send(worker, "Welcome to the Twitter")
+        #GenServer.cast(:myClient, {:initial, data, worker})
+        case Map.get(data, "function") do
+           "hashtag" -> GenServer.cast(:myClient, {:hashtag, Map.get(data, "hashtag"), socket})
+           "mention" -> GenServer.cast(:myClient, {:mention, Map.get(data, "mention"), socket})
+           "tweet" -> GenServer.cast(:myClient, {:tweet, username, Map.get(data, "sender"), Map.get(data, "tweet"), socket})
+           "subscribe" -> GenServer.cast(:myClient, {:subscribe, data["username"], data["follow"]})
+           "unsubscribe" -> GenServer.cast()
+        end
+        listen(socket, username)
+    end
 
     defp perform_logout(server, username) do
         # send logout message
@@ -89,26 +110,40 @@ defmodule Client do
         # TODO:- get the feed
     end
 
-    defp perform_registration(server) do
-        # pick a username
-        username = 'akshayt80'
+    defp perform_registration(server, username \\ 'akshayt80') do
         # send register message to server
         data = %{'function'=> 'register', 'username'=> username}
         data = blocking_send_message(server, data)
         if Map.get(data, "status") != "success" do
             Logger.debug "No success while registering"
+            perform_registration(server, generate_random_str())
         end
         # send login message to server
         data = %{'function'=> 'login', 'username'=> username}
-        send_message(server, data)
+        # expects servers response to proceed
+        response = blocking_send_message(server, data)
+
         # send subscriber message to server
+        users = data["users"]
+        Logger.info "Current users: #{inspect(users)}"
+        # take user input for subscribing to some users or pick some random users to subscribe
+        input = IO.gets "Enter user to subscribe to space separated? "
+        #friends = input |> String.split([" ", "\n"], trim: true)
+        friend = String.trim(input, "\n")
+        data = %{'function'=> 'subscribe', 'username'=> username, 'follow'=> friend}
+        send_message(server, data)
+    end
+
+    defp perform_first_login(server, users) do
+        # randomly pick some user to subscribe to
     end
 
     defp received_tweet(server, username, tweet) do
         # print tweet
-        Logger.info "#{tweet}"
+        Logger.info "username:#{username} incoming tweet:- #{tweet}"
         # with probability od 10% do retweet
         if :rand.uniform(100) <= 10 do
+            Logger.debug "username:#{username} doing retweet"
             data = %{'function'=> 'tweet', 'username'=> username, 'tweet'=> tweet}
             send_message(server, data)
         end
@@ -125,5 +160,17 @@ defmodule Client do
         data = %{'function'=> 'hashtags', 'hashtag'=> username}
         send_message(server, data)
         # print all the tweets with hashtag
+    end
+
+
+    #############################
+    # Client utility functions
+    #############################
+
+    defp generate_random_str(len \\ 9) do
+        common_str = "abcdefghijklmnopqrstuvwxyz0123456789"
+        list = common_str |> String.split("", trim: true) |> Enum.shuffle
+        random_str = 1..len |> Enum.reduce([], fn(_, acc) -> [Enum.random(l) | acc] end) |> Enum.join("")
+        random_str
     end
 end
