36197f33327bd8738e314de01493831391277067
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2017-11-26 20:14:34 -0500

    initial working client code, added some utility functions for interactive mode

--- project4part1/lib/client.ex
+++ project4part1/lib/client.ex
@@ -1,29 +1,57 @@
 defmodule Client do
     use GenServer
     require Logger
-    def start_link(server_ip, port) do
+    def start_link(server_ip, port, mode \\ :interactive) do
         # Connect to server
         Logger.debug "Establishing Server connection"
         {:ok, socket} = :gen_tcp.connect(server_ip, port, [:binary, {:active, false},{:packet, 0}])
         Logger.debug "Server Connection Established"
+        username = IO.gets "Enter username: "
+        username = String.trim(username)
+        perform_registration(socket, username)
         #spawn fn -> start_communication(socket, self())end
         # create clients and assign neigbors to them
         # TODO:- before first step we must register the user at server
         GenServer.start_link(__MODULE__, %{'tweets'=> [], 'subscribers'=> MapSet.new}, name: :myClient)
         #start_communication(socket, self())
         # TODO:- remove hardcoded username
-        spawn fn -> listen(socket, "akshayt80") end
+        spawn fn -> listen(socket, username) end
+        if mode == :interactive do
+            interactive_client(socket, username)
+        end
+    end
+    defp interactive_client(socket, username) do
+        option = IO.gets "Options:\n1. Tweet\n2. Hashtag query\n3. Mention query\n4. Subscribe\n5. unsubscribe\n6. Login\n7. Logout\nEnter your choice: "
+        case String.trim(option) do
+            # "1" -> username = IO.gets "Enter username: "
+            #         username = String.trim(username)
+            #         perform_registration(socket, username)
+            "1" -> tweet = IO.gets "Enter tweet: "
+                    send_tweet(socket, String.trim(tweet), username)
+            "2" -> hashtag = IO.gets "Enter hashtag to query for: "
+                    hashtag_query(socket, String.trim(hashtag), username)
+            "3" -> mention = IO.gets "Enter the username(add @ in begining) to look for: "
+                    mention_query(socket, String.trim(mention), username)
+            "4" -> user = IO.gets "Enter the username you want to follow: "
+                    subscribe(socket, String.split(user, [" ", "\n"], trim: true), username)
+            "5" -> user = IO.gets "Enter the username you want to unsubscribe: "
+                    unsubscribe(socket, String.split(user, [" ", "\n"], trim: true), username)
+            "6" -> perform_login(socket, username)
+            "7" -> perform_logout(socket, username)
+            _ -> IO.puts "Invalid option. Please try again"
+        end
+        interactive_client(socket, username)
     end
     #TODO:- write a function which creates simulation for client
-    defp start_communication(socket, parent) do
-        Logger.debug "sending Hello"
-        raw_data = %{"function" => :register, "reply"=> true, "data"=> %{}}
-        data = Poison.encode!(raw_data)
-        socket |> :gen_tcp.send(data)
-        # Receive k value from server
-        {:ok, data} = :gen_tcp.recv(socket, 0)
-        Logger.debug "received message from server: #{data}"
-    end
+    # defp start_communication(socket, parent) do
+    #     Logger.debug "sending Hello"
+    #     raw_data = %{"function" => :register, "reply"=> true, "data"=> %{}}
+    #     data = Poison.encode!(raw_data)
+    #     socket |> :gen_tcp.send(data)
+    #     # Receive k value from server
+    #     {:ok, data} = :gen_tcp.recv(socket, 0)
+    #     Logger.debug "received message from server: #{data}"
+    # end
 
     def init(map) do
         {:ok, map}
@@ -35,7 +63,9 @@ defmodule Client do
     end
 
     def receive_message(receiver) do
+        # TODO:- do thorough testing of this function as it sometimes causes error
         {:ok, data} = :gen_tcp.recv(receiver, 0)
+        Logger.debug "Received message: #{data}"
         Poison.decode!(data)
     end
 
@@ -54,25 +84,36 @@ defmodule Client do
         {:noreply, map}
     end
 
-    def handle_cast({:login, }, map) do
-       {:noreply, map} 
-    end
-
-    def handle_cast({:logout}, map) do
+    def handle_cast({:mention, tweets}, map) do
+        for tweet <- tweets do
+            Logger.info "Tweet: #{tweet}"
+        end
         {:noreply, map}
     end
 
-    def handle_cast({:mention}, map) do
-        {:noreply, map}
-    end
-
-    def handle_cast({:hashtag}, map) do
+    def handle_cast({:hashtag, tweets}, map) do
+        for tweet <- tweets do
+            Logger.info "Tweet: #{tweet}"
+        end
         {:noreply, map}
     end
 
     def handle_cast({:tweet, username, sender, tweet, socket}, map) do
-        Logger.debug "user: #{username} sender: #{sender} tweet: #{tweet}"
-        received_tweet(socket, username, tweet)
+        Logger.info "username:#{username} sender: #{sender} incoming tweet:- #{tweet}"
+        # with probability od 10% do retweet
+        if :rand.uniform(100) <= 10 do
+            Logger.debug "username:#{username} doing retweet"
+            data = %{"function"=> "tweet", "username"=> username, "tweet"=> tweet}
+            send_message(socket, data)
+        end
+        {:noreply, map}
+    end
+
+    def handle_cast({:feed, feed}, map) do
+        Logger.debug "Incoming feed which was accumulated while you were offline"
+        for item <- feed do
+            Logger.info "Tweet: #{item}"
+        end
         {:noreply, map}
     end
 
@@ -87,60 +128,65 @@ defmodule Client do
         #Logger.debug "sending initial message"
         #:gen_tcp.send(worker, "Welcome to the Twitter")
         #GenServer.cast(:myClient, {:initial, data, worker})
-        case Map.get(data, "function") do
-           "hashtag" -> GenServer.cast(:myClient, {:hashtag, Map.get(data, "hashtag"), socket})
-           "mention" -> GenServer.cast(:myClient, {:mention, Map.get(data, "mention"), socket})
-           "tweet" -> GenServer.cast(:myClient, {:tweet, username, Map.get(data, "sender"), Map.get(data, "tweet"), socket})
-           "subscribe" -> GenServer.cast(:myClient, {:subscribe, data["username"], data["users"]})
-           "unsubscribe" -> GenServer.cast(:myClient, {:unsubscribe, data["username"], data["users"]})
-           "feed" -> spawn fn -> process_feed(data["feed"]) end
+        case data["function"] do
+           "hashtag" -> GenServer.cast(:myClient, {:hashtag, data["tweets"]})
+           "mention" -> GenServer.cast(:myClient, {:mention, data["tweets"]})
+           "tweet" -> GenServer.cast(:myClient, {:tweet, username, data["sender"], data["tweet"], socket})
+           "feed" -> GenServer.cast(:myClient, {:feed, data["feed"]})
         end
         listen(socket, username)
     end
 
-    def perform_logout(server, username) do
+    def perform_logout(server, username, autologin \\ false) do
         # send logout message
-        data = %{'function'=> 'logout', 'username'=> username}
+        data = %{"function"=> "logout", "username"=> username}
         send_message(server, data)
         # sleep for some random time between 1 to 10 sec
-        sec = :rand.uniform(10) * 1000
-        Logger.debug "#{username} sleeping for #{sec}"
-        :timer.sleep sec
-        # send login back to server
-        data = %{'function'=> 'login', 'username'=> username}
-        send_message(server, data)
+        if autologin do
+            sec = :rand.uniform(10) * 1000
+            Logger.debug "#{username} sleeping for #{sec} seconds"
+            :timer.sleep sec
+            # send login back to server
+            perform_login(server, username)
+        end
+        #response = blocking_send_message(server, data)
+        #Logger.info "Tweets while you were offline: #{inspect(response["tweets"])}"
         # print the feed
         # TODO:- get the feed
+    end
+
+    defp perform_login(server, username) do
+        data = %{"function"=> "login", "username"=> username}
+        Logger.debug "Sending login message to server"
+        send_message(server, data)
     end
 
     def perform_registration(server, username \\ "akshayt80") do
         # send register message to server
         data = %{"function"=> "register", "username"=> username}
         data = blocking_send_message(server, data)
-        if Map.get(data, "status") != "success" do
+        if data["status"] != "success" do
             Logger.debug "No success while registering"
             #perform_registration(server, generate_random_str())
         else
             # send login message to server
-            data = %{"function"=> "login", "username"=> username}
-            # expects servers response to proceed
-            send_message(server, data)
+            send_message(server, %{"function"=> "login", "username"=> username})
 
             # send subscriber message to server
             users = data["users"]
-            Logger.info "Current users: #{inspect(users)}"
+            Logger.info "Current users at server: #{inspect(users)}"
             # take user input for subscribing to some users or pick some random users to subscribe
-            input = IO.gets "Enter user to subscribe to space separated? "
-            friends = input |> String.split([" ", "\n"], trim: true)
+            #input = IO.gets "Enter user to subscribe to space separated? "
+            #friends = input |> String.split([" ", "\n"], trim: true)
             #friend = String.trim(input, "\n")
-            data = %{"function"=> "subscribe", "username"=> username, "users"=> friends}
-            send_message(server, data)
+            #data = %{"function"=> "subscribe", "username"=> username, "users"=> friends}
+            #send_message(server, data)
         end
     end
 
-    defp perform_first_login(server, users) do
-        # randomly pick some user to subscribe to
-    end
+    # defp perform_first_login(server, users) do
+    #     # randomly pick some user to subscribe to
+    # end
 
     def received_tweet(server, username, tweet) do
         # print tweet
@@ -148,7 +194,7 @@ defmodule Client do
         # with probability od 10% do retweet
         if :rand.uniform(100) <= 10 do
             Logger.debug "username:#{username} doing retweet"
-            data = %{'function'=> 'tweet', 'username'=> username, 'tweet'=> tweet}
+            data = %{"function"=> "tweet", "username"=> username, "tweet"=> tweet}
             send_message(server, data)
         end
     end
@@ -160,19 +206,43 @@ defmodule Client do
         end
     end
 
-    defp query_server(server, username, key) do
-        # no need to do this in simulator
-        # send message to server to get mentions
-        data = %{'function'=> 'mention', 'name'=> username}
-        send_message(server, data)
-        # print all the tweets with mention
-        # TODO:- server is not sending back the tweets from feed
-        # send message to server to get hashtags
-        data = %{'function'=> 'hashtags', 'hashtag'=> username}
-        send_message(server, data)
-        # print all the tweets with hashtag
+    # defp query_server(server, username, key) do
+    #     # no need to do this in simulator
+    #     # send message to server to get mentions
+    #     data = %{'function'=> 'mention', 'name'=> username}
+    #     send_message(server, data)
+    #     # print all the tweets with mention
+    #     # TODO:- server is not sending back the tweets from feed
+    #     # send message to server to get hashtags
+    #     data = %{'function'=> 'hashtags', 'hashtag'=> username}
+    #     send_message(server, data)
+    #     # print all the tweets with hashtag
+    # end
+
+    defp send_tweet(socket, tweet, username) do
+        data = %{"function"=> "tweet", "username"=> username, "tweet"=> tweet}
+        send_message(socket, data)
     end
 
+    defp hashtag_query(socket, hashtag, username) do
+        data = %{"function"=> "hashtag", "username"=> username, "hashtag"=> hashtag}
+        send_message(socket, data)
+    end
+
+    defp mention_query(socket, mention, username) do
+        data = %{"function"=> "mention", "mention"=> mention, "username"=> username}
+        send_message(socket, data)
+    end
+
+    defp subscribe(socket, users, username) do
+        data = %{"function"=> "subscribe", "users"=> users, "username"=> username}
+        send_message(socket, data)
+    end
+
+    defp unsubscribe(socket, users, username) do
+        data = %{"function"=> "unsubscribe", "users"=> users, "username"=> username}
+        send_message(socket, data)
+    end
 
     #############################
     # Client utility functions
