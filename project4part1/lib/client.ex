1a73f621a68bc859ee8a5df4a27066ce4bb5235e
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2017-11-29 02:16:34 -0500

    working client with simulator functionalitites

--- project4part1/lib/client.ex
+++ project4part1/lib/client.ex
@@ -1,25 +1,110 @@
 defmodule Client do
     use GenServer
     require Logger
-    def start_link(server_ip, port, mode \\ :interactive) do
+    def simulate(server_ip, port, user_count \\ 3) do
+        user_set = 1..user_count |> Enum.reduce(MapSet.new, fn(x, acc) -> MapSet.put(acc, "user_#{x}") end)
+        constant = zipf_constant(user_count)
+        Logger.debug "zipf constant: #{constant}"
+        # Top 10%  and bootom 10% of total
+        high = low = round(:math.ceil(user_count * 0.1))
+        for n <- 1..user_count do
+            username = "user_#{n}"
+            available_subscribers = MapSet.difference(user_set, MapSet.new([username]))
+            subscriber_count = zipf_prob(constant, n, user_count)
+            Logger.debug "user: user_#{n} subscriber_count: #{subscriber_count}"
+            subscribers = get_subscribers(available_subscribers, subscriber_count)
+            frequency = :medium
+            if n <= high do
+                frequency = :high
+            end
+            if n >= low do
+              frequency = :low
+            end
+            spawn fn -> start_link(server_ip, port, :simulate, username, subscribers, frequency) end
+        end
+        keep_alive()
+    end
+    def keep_alive() do
+        :timer.sleep 10000
+        keep_alive()
+    end
+    # number represents number of user which is used in username in simulation mmode
+    # users is list of all available users
+    # frequency if :high then every 200 ms one tweet will be sent, :medium every 700ms and :slow every 1200ms
+    def start_link(server_ip, port, mode \\ :interactive, username \\ None, users \\ None, frequency \\ :medium) do
         # Connect to server
         Logger.debug "Establishing Server connection"
         {:ok, socket} = :gen_tcp.connect(server_ip, port, [:binary, {:active, false},{:packet, 0}])
         Logger.debug "Server Connection Established"
-        username = IO.gets "Enter username: "
-        username = String.trim(username)
+        if mode == :interactive do
+            username = IO.gets "Enter username: "
+            username = String.trim(username)
+        else
+            #username = "user_#{number}"
+            Logger.debug "username given #{username} with frequency:#{frequency}"
+        end
         perform_registration(socket, username)
-        #spawn fn -> start_communication(socket, self())end
-        # create clients and assign neigbors to them
-        # TODO:- before first step we must register the user at server
-        GenServer.start_link(__MODULE__, %{'tweets'=> [], 'subscribers'=> MapSet.new}, name: :myClient)
-        #start_communication(socket, self())
-        # TODO:- remove hardcoded username
+
+        if mode == :simulate do
+            #:timer.sleep 1000
+            # subscribe to users
+            Logger.debug "performing bulk_subscription for user: #{username} followers: #{inspect(users)}"
+            bulk_subscription(socket, users, username)
+        end
+
+        GenServer.start_link(__MODULE__, %{"mode"=> mode, "retweet_prob"=> 10}, name: :"#{username}")
         spawn fn -> listen(socket, username) end
         if mode == :interactive do
             interactive_client(socket, username)
+        else
+            simulative_client(socket, username, frequency)
         end
     end
+
+    defp simulative_client(socket, username, frequency) do
+        #send tweet
+        tweet = generate_random_str(100)
+        Logger.debug "#{username} sending tweet: #{tweet}"
+        send_tweet(socket, tweet, username)
+        #sleep
+        # perform logout
+        if frequency == :high do
+            :timer.sleep(200)
+            simulate_logout(socket, username, frequency)
+        else
+            if frequency == :medium do
+                :timer.sleep(700)
+                simulate_logout(socket, username, frequency)
+            else
+                :timer.sleep(1200)
+                simulate_logout(socket, username, frequency)
+            end
+        end
+        simulative_client(socket, username, frequency)
+    end
+
+    defp get_subscribers(available_subscribers, subscriber_count) do
+        Enum.shuffle(available_subscribers) |> Enum.take(subscriber_count) |> MapSet.new()
+    end
+
+    defp simulate_logout(socket, username, frequency) do
+        random_num = :rand.uniform(100)
+        if frequency == :high and random_num <= 3 do
+            # autologin is turned true
+            perform_logout(socket, username, true)
+        else
+            if frequency == :medium and random_num <= 5 do
+                # autologin is turned true
+                perform_logout(socket, username, true)
+            else
+                if random_num <= 7 do
+                    # autologin is turned true
+                    perform_logout(socket, username, true)
+                end
+            end
+        end
+    end
+
     defp interactive_client(socket, username) do
         option = IO.gets "Options:\n1. Tweet\n2. Hashtag query\n3. Mention query\n4. Subscribe\n5. unsubscribe\n6. Login\n7. Logout\nEnter your choice: "
         case String.trim(option) do
@@ -101,10 +186,20 @@ defmodule Client do
     def handle_cast({:tweet, username, sender, tweet, socket}, map) do
         Logger.info "username:#{username} sender: #{sender} incoming tweet:- #{tweet}"
         # with probability od 10% do retweet
-        if :rand.uniform(100) <= 10 do
+        mode = map["mode"]
+        if mode != :interactive and :rand.uniform(100) <= map["retweet_prob"] do
             Logger.debug "username:#{username} doing retweet"
             data = %{"function"=> "tweet", "username"=> username, "tweet"=> tweet}
             send_message(socket, data)
+        end
+        if mode == :interactive do
+            input = IO.gets "Want to retweet(y/n)? "
+            input = String.trim(input)
+            if input == "y" do
+                Logger.debug "username:#{username} doing retweet"
+                data = %{"function"=> "tweet", "username"=> username, "tweet"=> tweet}
+                send_message(socket, data)
+            end
         end
         {:noreply, map}
     end
@@ -120,19 +215,26 @@ defmodule Client do
     ####################
 
     def listen(socket, username) do
-        {:ok, data} = :gen_tcp.recv(socket, 0)
-        data = Poison.decode!(data)
-        Logger.debug "received data at user #{username} data: #{inspect(data)}"
+        {status, response} = :gen_tcp.recv(socket, 0)
+        if status == :ok do
+            # this will handle the case when there are more than one
+            multiple_data = response |> String.split("}", trim: :true)
+            for data <- multiple_data do
+                Logger.debug "data to be decoded: #{inspect(data)}"
+                data = Poison.decode!("#{data}}")
+                Logger.debug "received data at user #{username} data: #{inspect(data)}"
 
-        # Send value of k as String
-        #Logger.debug "sending initial message"
-        #:gen_tcp.send(worker, "Welcome to the Twitter")
-        #GenServer.cast(:myClient, {:initial, data, worker})
-        case data["function"] do
-           "hashtag" -> GenServer.cast(:myClient, {:hashtag, data["tweets"]})
-           "mention" -> GenServer.cast(:myClient, {:mention, data["tweets"]})
-           "tweet" -> GenServer.cast(:myClient, {:tweet, username, data["sender"], data["tweet"], socket})
-           "feed" -> GenServer.cast(:myClient, {:feed, data["feed"]})
+                # Send value of k as String
+                #Logger.debug "sending initial message"
+                #:gen_tcp.send(worker, "Welcome to the Twitter")
+                #GenServer.cast(:myClient, {:initial, data, worker})
+                case data["function"] do
+                   "hashtag" -> GenServer.cast(:"#{username}", {:hashtag, data["tweets"]})
+                   "mention" -> GenServer.cast(:"#{username}", {:mention, data["tweets"]})
+                   "tweet" -> GenServer.cast(:"#{username}", {:tweet, username, data["sender"], data["tweet"], socket})
+                   "feed" -> GenServer.cast(:"#{username}", {:feed, data["feed"]})
+                end
+            end
         end
         listen(socket, username)
     end
@@ -170,7 +272,7 @@ defmodule Client do
             #perform_registration(server, generate_random_str())
         else
             # send login message to server
-            send_message(server, %{"function"=> "login", "username"=> username})
+           # send_message(server, %{"function"=> "login", "username"=> username})
 
             # send subscriber message to server
             users = data["users"]
@@ -239,6 +341,11 @@ defmodule Client do
         send_message(socket, data)
     end
 
+    defp bulk_subscription(socket, users, username) do
+        data = %{"function"=> "bulk_subscription", "users"=> users, "username"=> username}
+        send_message(socket, data)
+    end
+
     defp unsubscribe(socket, users, username) do
         data = %{"function"=> "unsubscribe", "users"=> users, "username"=> username}
         send_message(socket, data)
@@ -248,10 +355,21 @@ defmodule Client do
     # Client utility functions
     #############################
 
-    defp generate_random_str(len \\ 9) do
-        common_str = "abcdefghijklmnopqrstuvwxyz0123456789"
+    defp generate_random_str(len) do
+        common_str = "  abcdefghijklmnopqrstuvwxyz  0123456789"
         list = common_str |> String.split("", trim: true) |> Enum.shuffle
         random_str = 1..len |> Enum.reduce([], fn(_, acc) -> [Enum.random(list) | acc] end) |> Enum.join("")
         random_str
+    end
+
+    defp zipf_constant(users) do
+
+        # c = (Sum(1/i))^-1 where i = 1,2,3....n
+        users = for n <- 1..users, do: 1/n
+        :math.pow(Enum.sum(users), -1)
+    end
+    defp zipf_prob(constant, user, users) do
+        # z=c/x where x = 1,2,3...n
+        round((constant/user)*users)
     end
 end
